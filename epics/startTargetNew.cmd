d84ac53707ff6c8acab7199f83bb77a957bcb658
Anders Sandstrom <anders.sandstrom@esss.se>
2022-04-01 09:59:51 +0200

    Update new ip and ams to connect in B02

--- epics/startTargetNew.cmd
+++ epics/startTargetNew.cmd
@@ -22,7 +22,7 @@ epicsEnvSet("SAMPLE_RATE_MS_DEF", "500");
 epicsEnvSet("MAX_DLY_MS_DEF",     "100");
 
 #adsSetLocalAddress "172.30.2.35.1.1"
-adsAsynPortDriverConfigure("${ADS_ASYN_PORT}","172.30.2.35","172.30.2.35.1.1",${ADSPORT_DEF},1000,0,0,${SAMPLE_RATE_MS_DEF},${MAX_DLY_MS_DEF},1000,0)
+adsAsynPortDriverConfigure("${ADS_ASYN_PORT}","172.30.12.18","172.30.12.18.1.1",${ADSPORT_DEF},1000,0,0,${SAMPLE_RATE_MS_DEF},${MAX_DLY_MS_DEF},1000,0)
 #adsSetLocalAddress "172.30.2.52.1.1"
 
 asynOctetSetOutputEos("${ADS_ASYN_PORT}", -1, "\n")
